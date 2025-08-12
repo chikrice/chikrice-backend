ec5ec681597939323e480a425b9b2b905074be40
Khaled <khaledjavedan76@gmail.com>
2025-08-12 16:17:08 +0330

    chore: cleaned up unused files from roadmap services

--- src/services/roadmap/utils/calc-activity-level.js
+++
@@ -1,7 +0,0 @@
-const calcActivityLevel = (activityLevel) => {
-  const activityMultiplier = [1.2, 1.375, 1.55, 1.725, 1.9][activityLevel - 1];
-
-  return activityMultiplier;
-};
-
-module.exports = calcActivityLevel;
--- src/services/roadmap/utils/calc-bmr.js
+++
@@ -1,8 +0,0 @@
-const calcBMR = (weight, height, age, gender) => {
-  if (gender === 'male') {
-    return 10 * weight + 6.25 * height - 5 * age + 5;
-  }
-  return 10 * weight + 6.25 * height - 5 * age - 161;
-};
-
-module.exports = calcBMR;
